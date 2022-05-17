e5a56a9dc0347f5aab2f0357488653a057aaec8e
7z.henry <henry.horlebein@gmail.com>
2022-05-17 13:45:03 +0200

    push

--- Scarlet Gen.py
+++ Scarlet Gen.py
@@ -9,10 +9,11 @@ icon = input(("App Icon Link: "))
 download = input(("iPA Download Link: "))
 description = input(("Apps Description: "))
 bundleID = input(("Whats the Bundle ID ?: "))
+banner = input(("Whats the banner?: "))
 
 os.system("cls")
 
-end = '{\n    "name": "'+name+'",\n    "version": "'+version+'",\n    "icon": "'+icon+'",\n    "down": "'+download+'",\n    "category": "Tweaked Apps",\n    "description": "'+description+'",\n    "bundleID": "'+bundleID+'",\n    "appstore": "'+bundleID+'",\n"contact":  {\n        "web": "https://7zz.shop/discord",\n        "twitter": "https://twitter.com/7z_henry"}}'
+end = '{\n    "name": "'+name+'",\n    "version": "'+version+'",\n    "icon": "'+icon+'",\n    "down": "'+download+'",\n    "dev": "7z",\n    "category": "Tweaked Apps",\n    "description": "'+description+'",\n    "bundleID": "'+bundleID+'",\n    "appstore": "'+bundleID+'",\n    "banner": "'+banner+'",\n"contact":  {\n        "web": "https://7zz.shop/discord",\n        "twitter": "https://twitter.com/7z_henry"}}'
 
 print(end)
 
